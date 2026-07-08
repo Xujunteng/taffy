ca3f6ed350a652dabc64e39ebe836a3823217560
徐骏腾 <2562187986@qq.com>
2026-07-08 19:50:47 +0800

    Delete taffy-frontend-galaxy directory

--- taffy-frontend-galaxy/src/components/EmptyState.vue
+++
@@ -1,2 +0,0 @@
-<template><div class="empty-box"><el-empty :description="description"><slot /></el-empty></div></template>
-<script setup>defineProps({ description: { type: String, default: '暂无数据' } })</script>
--- taffy-frontend-galaxy/src/components/ErrorBlock.vue
+++
@@ -1,2 +0,0 @@
-<template><el-alert v-if="message" :title="message" type="error" show-icon :closable="false" style="margin-bottom:12px" /></template>
-<script setup>defineProps({ message: String })</script>
